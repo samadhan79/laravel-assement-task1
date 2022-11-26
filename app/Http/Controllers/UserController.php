44bc6c46f0121f3b677ecc82277d3fe042f80f5f
sam <samalexe79@gmail.com>
2022-11-26 15:10:59 +0530

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Exception;
+use Laravel\Sanctum\HasApiTokens;
+
+class UserController extends Controller
+{
+    function redirectToGoogle(Request $request)
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    function handleGoogleCallback(Request $request)
+    {
+     try {
+        $user = Socialite::driver('google')->stateless()->user();
+
+        $finduser = User::where('google_id', $user->id)->first();
+        
+        if($finduser){
+            Auth::login($finduser);
+            return redirect()->intended('dashboard');
+        }else{
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_id'=> $user->id,
+                'password' => encrypt('123456dummy')
+            ]);
+
+            Auth::login($newUser);
+
+            return redirect()->intended('dashboard');
+        }
+
+    } catch (Exception $e) {
+        dd($e->getMessage());
+    }
+}
+
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Login');
+});
+
+Route::get('auth/google', [UserController::class, 'redirectToGoogle'])->name('login');
+Route::get('callback', [UserController::class, 'handleGoogleCallback'])->name('callback');
+Route::get('dashboard', [HomeController::class, 'Dashboard'])->name('dashboard');
